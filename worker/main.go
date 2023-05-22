383f1a116a06a0688cf17dec997189b7a8b86ef9
Juan Pablo Valencia <juan.valencia10a@gmail.com>
2023-05-22 13:43:14 +0300

    move run to main

--- worker/main.go
+++ worker/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"flag"
+	"fmt"
 	"log"
+	"os/exec"
 	"time"
 
 	pb "thesis/poc/proto"
@@ -16,6 +19,32 @@ import (
 var (
 	controllerAddress = flag.String("controller", "localhost:50051", "The address to connect to")
 )
+
+func runJob(param string) string {
+	dockerCommand := []string{"run"}
+
+	if len(param) > 0 {
+		dockerCommand = append(dockerCommand, "-e", fmt.Sprintf("MESSAGE=%v", param))
+	}
+	dockerCommand = append(dockerCommand, "worker")
+
+	cmd := exec.Command("docker", dockerCommand...)
+
+	var cmdOut bytes.Buffer
+	var cmdErr bytes.Buffer
+	cmd.Stdout = &cmdOut
+	cmd.Stderr = &cmdErr
+
+	err := cmd.Run()
+
+	if err != nil {
+		log.Print(cmdErr.String())
+		log.Fatal(err)
+	}
+
+	log.Printf("Container output: %s\n", cmdOut.String())
+	return cmdOut.String()
+}
 
 func main() {
 	flag.Parse()
