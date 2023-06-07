bc87b5eeef4e230e36266d2df27534da08cf88ae
Juan Pablo Valencia <juan.valencia10a@gmail.com>
2023-06-07 15:46:57 +0300

    type improvement

--- controller/types.go
+++ controller/types.go
@@ -10,13 +10,15 @@ const (
 	Pending    StatusType = "pending"
 	InProgress StatusType = "in progress"
 	Completed  StatusType = "completed"
+	Failed     StatusType = "failed"
 )
 
 type Job struct {
 	Id        string     `json:"id"`
-	Arguments string     `json:"arguments"`
+	Arguments string     `json:"-"`
 	Status    StatusType `json:"status"`
 	Result    string     `json:"result,omitempty"`
+	Error     string     `json:"error,omitempty"`
 }
 
 type CreateResponse []struct {
